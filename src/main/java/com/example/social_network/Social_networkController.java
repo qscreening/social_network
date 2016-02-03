c3e5b77c45748623b86f2c26f556bf61d1b8b323
ror <chaitanyaeluri@gmail.com>
2016-02-03 11:49:16 +0530

    pom file modified

--- src/main/java/com/example/social_network/Social_networkController.java
+++ src/main/java/com/example/social_network/Social_networkController.java
@@ -1,24 +1,36 @@
-package com.example.social_network;
 
+package com.example.hibernate;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
 import org.hibernate.Session;
 import java.util.List;
 import org.hibernate.Query;
 import java.util.Iterator; 
-
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 @RestController
 public class Social_networkController {
-	@RequestMapping("/Social_networkController")
- 	 public List network() {
+	@RequestMapping("/users")
+ 	 public List network(HttpServletRequest request,HttpServletResponse response)
+           throws Exception {
+		/* System.out.println("entering into social_networkController");
+		 int id=Integer.parseInt(request.getParameter("id"));
+		 String tableName=request.getParameter("tableName");
+		 ArrayList l=new ArrayList();
+		 l.add(id);
+		 l.add(tableName);*/
                 Session session = HibernateSessionManager.getSessionFactory().openSession();
+		System.out.println(session);
 		session.beginTransaction();
-		Query query = session.createQuery("from users");
+		//String qry="from "+ tableName+" where id="+id;
+		Query query = session.createQuery("from posts");
 		List list = query.list();
-		session.save(list);
+		//session.save(list);
 		session.getTransaction().commit();
+		String str="chaitanya";
 		return list;
 	}
 }
