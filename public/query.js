dbb6c68cf6bfab17718e363c18b5b16a39d17ea4
ror <chaitanyaeluri@gmail.com>
2016-02-09 17:37:09 +0530

    modify the js file

--- public/query.js
+++ public/query.js
@@ -1,9 +1,31 @@
+var text=function(){
+  var userName="";
+  var Password="";
+  $("#userName1").on("keyup", function(){
+  	userName = $("#userName1").val();
+      //alert(userName);
+   });	
+          
+  $("#password1").on("keyup", function(){
+       Password=$("#password1").val();
+       //alert(Password);
+   });
+   $("#btn1").click(function(){
+      alert(userName);	
+      $.ajax({
+	      url: "signin?userName="+userName+"&password="+Password, 
+	      method: "post",
+	      success: function(result){
+	      console.log(result);
+	    },error:function(){
+	   alert("error occured")
+	   }
+     });
+});
+ 
+}	
+       
+
 $(document).ready(function(){
-	$("#up").click(function(){
-	$.ajax({url: "signUp", success: function(result){
-	console.log(result);
-
-	} });
-  		}); 	
-
+    text();
 });
