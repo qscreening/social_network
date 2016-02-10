72f94517d34f3aebdc506cd1ed7a43f011acd863
ror <chaitanyaeluri@gmail.com>
2016-02-10 14:10:14 +0530

    html page modified

--- public/query.js
+++ public/query.js
@@ -1,31 +1,37 @@
 var text=function(){
-  var userName="";
-  var Password="";
-  $("#userName1").on("keyup", function(){
+	var userName="";
+  	var Password="";
+  	$("#userName1").on("keyup", function(){
   	userName = $("#userName1").val();
       //alert(userName);
-   });	
+	});	
           
-  $("#password1").on("keyup", function(){
-       Password=$("#password1").val();
+ 	$("#password1").on("keyup", function(){
+        Password=$("#password1").val();
        //alert(Password);
-   });
-   $("#btn1").click(function(){
-      alert(userName);	
-      $.ajax({
-	      url: "signin?userName="+userName+"&password="+Password, 
-	      method: "post",
-	      success: function(result){
-	      console.log(result);
-	    },error:function(){
-	   alert("error occured")
-	   }
-     });
-});
+   	});
+  	$("#btn1").click(function(){
+      	alert(userName);	
+	      	$.ajax({
+		      url: "signin", 
+		      data:{userName:userName,password:Password},
+		      method: "post",
+		      success: function(result){ 
+			      if(result){ 
+			      	window.location.href="home.html";
+			      	alert("loged in");
+			      }
+			      else{
+			      	alert("user not exist");
+			      }
+		      	
+		    	},error:function(){
+		    		alert("error occured")
+			 }
+	     	});
+ 	});
  
 }	
-       
-
 $(document).ready(function(){
     text();
 });
